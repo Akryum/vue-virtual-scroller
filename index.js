f5c9b764a0e77b5f58eeaa59f2bae5a3fc72176f
Stefan Buhrmester <buhrmi@gmail.com>
2017-08-02 20:07:53 +0900

    use correct ES2015 syntax (#31)

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export default from './dist/vue-virtual-scroller'
+export {default} from './dist/vue-virtual-scroller'
 export * from './dist/vue-virtual-scroller'
 import './dist/vue-virtual-scroller.css'
