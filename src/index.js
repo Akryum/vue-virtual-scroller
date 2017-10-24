716d20c330f043ceb5e96f780c944d80ec3d1df9
Guillaume Chau <alphadelta.fg@gmail.com>
2017-10-24 16:41:38 +0200

    Auto install

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import _VirtualScroller from './components/VirtualScroller.vue'
+import VirtualScroller from './components/VirtualScroller.vue'
 
-export function install (Vue) {
-  Vue.component('virtual-scroller', _VirtualScroller)
+export {
+  VirtualScroller,
 }
 
-export const VirtualScroller = _VirtualScroller
+function registerComponents (Vue, prefix) {
+  Vue.component(`${prefix}virtual-scroller`, VirtualScroller)
+}
 
-export default {
-  /* eslint-disable no-undef */
+const plugin = {
+  // eslint-disable-next-line no-undef
   version: VERSION,
-  install,
+  install (Vue, options) {
+    const finalOptions = Object.assign({}, {
+      installComponents: true,
+      componentsPrefix: '',
+    }, options)
+
+    if (finalOptions.installComponents) {
+      registerComponents(Vue, finalOptions.componentsPrefix)
+    }
+  },
+}
+
+export default plugin
+
+// Auto-install
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
 }
