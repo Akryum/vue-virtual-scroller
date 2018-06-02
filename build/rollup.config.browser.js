d217bf8acbd1277cd3fdfc61459d2f3c35f88974
Guillaume Chau <alphadelta.fg@gmail.com>
2018-06-02 13:30:25 +0200

    fix: browser build

--- build/rollup.config.browser.js
+++ build/rollup.config.browser.js
@@ -1,5 +1,5 @@
 import base from './rollup.config.base'
-import uglify from 'rollup-plugin-uglify'
+import { uglify } from 'rollup-plugin-uglify'
 import { minify } from 'uglify-es'
 
 const config = Object.assign({}, base, {
