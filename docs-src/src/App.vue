5ba314a9b52b1a41a82fcebcbe33cf0d08a23b7a
Guillaume Chau <alphadelta.fg@gmail.com>
2019-02-19 16:47:58 +0100

    docs(demo): simple list

--- docs-src/src/App.vue
+++ docs-src/src/App.vue
@@ -9,6 +9,7 @@
       <router-link :to="{ name: 'recycle' }">Recycle scroller demo</router-link>
       <router-link :to="{ name: 'dynamic' }">Dynamic scroller demo</router-link>
       <router-link :to="{ name: 'test-chat' }">Chat demo</router-link>
+      <router-link :to="{ name: 'simple-list' }">Simple array</router-link>
     </nav>
     <router-view class="page"/>
   </div>
--- docs-src/src/router.js
+++ docs-src/src/router.js
@@ -5,6 +5,7 @@ import Home from './components/Home.vue'
 import Recycle from './components/RecycleScrollerDemo.vue'
 import Dynamic from './components/DynamicScrollerDemo.vue'
 import TestChat from './components/TestChat.vue'
+import SimpleList from './components/SimpleList.vue'
 
 Vue.use(VueRouter)
 
@@ -14,5 +15,6 @@ export default new VueRouter({
     { path: '/recycle', name: 'recycle', component: Recycle },
     { path: '/dynamic', name: 'dynamic', component: Dynamic },
     { path: '/test-chat', name: 'test-chat', component: TestChat },
+    { path: '/simple-list', name: 'simple-list', component: SimpleList },
   ],
 })
