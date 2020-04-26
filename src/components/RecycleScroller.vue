656308fe80d69a8054b443fb2d62e2308eb2e88a
Damir <damir89@gmail.com>
2020-04-26 15:03:19 +0200

    feature: Skip vue when assigning hover class

--- src/components/RecycleScroller.vue
+++ src/components/RecycleScroller.vue
@@ -28,9 +28,8 @@
         :key="view.nr.id"
         :style="ready ? { transform: `translate${direction === 'vertical' ? 'Y' : 'X'}(${view.position}px)` } : null"
         class="vue-recycle-scroller__item-view"
-        :class="{ hover: hoverKey === view.nr.key }"
-        @mouseenter="hoverKey = view.nr.key"
-        @mouseleave="hoverKey = null"
+        @mouseenter="$event.target.classList.add('hover')"
+        @mouseleave="$event.target.classList.remove('hover')"
       >
         <slot
           :item="view.item"
