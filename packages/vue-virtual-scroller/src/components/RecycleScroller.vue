0b199d14c846ecc00b93f989adbe29961dc68aad
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-12-14 10:43:50 +0100

    fix: sorting views not working, #772

--- packages/vue-virtual-scroller/src/components/RecycleScroller.vue
+++ packages/vue-virtual-scroller/src/components/RecycleScroller.vue
@@ -737,7 +737,7 @@ export default {
     },
 
     sortViews () {
-      this.pool.sort((viewA, viewB) => viewA.index - viewB.index)
+      this.pool.sort((viewA, viewB) => viewA.nr.index - viewB.nr.index)
     },
   },
 }
